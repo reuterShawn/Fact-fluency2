b31e37e34ee915223416381f720fc56b2a7b47a9
Shawn Reuter <reutershawn45@gmail.com>
2026-01-07 12:57:33 -0600

    Add host

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
+export default {
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    hmr: {
+      host: 'gertrud-overelegant-abiogenetically.ngrok-free.dev',
+      port: 443,
+      protocol: 'https'
+    }
   }
-})
+}
