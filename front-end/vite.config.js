e38c1e31c70aebd1d62188ddcc5750d96e7491cd
Shawn Reuter <reutershawn45@gmail.com>
2026-01-11 15:24:37 -0600

    Fix app

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default {
+export default defineConfig({
+  plugins: [vue()],
   server: {
     host: '0.0.0.0',
     port: 5173,
-    hmr: {
-      host: 'gertrud-overelegant-abiogenetically.ngrok-free.dev',
-      port: 443,
-      protocol: 'https'
-    }
+    // hmr: {
+    //   host: 'gertrud-overelegant-abiogenetically.ngrok-free.dev',
+    //   port: 443,
+    //   protocol: 'https'
+    // }
   }
-}
+})
